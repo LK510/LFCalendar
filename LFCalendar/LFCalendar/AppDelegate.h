65df7885df3e5031fd06e0f1afb2e3cf1ca7e006
路飞 <lufei@anybeen.net>
2017-05-09 10:06:57 +0800

    Create Project

---
+++ LFCalendar/LFCalendar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LFCalendar
+//
+//  Created by 路飞 on 2017/5/9.
+//  Copyright © 2017年 路飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
